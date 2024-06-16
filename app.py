4c344ce6555fccc8378d61ee2e6ca1e7b22eebee
Imelda <imeldaatieno01@gmail.com>
2024-06-16 13:49:18 +0300

    Rendered html template

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
-app = Flask(__name__)
+app =Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
